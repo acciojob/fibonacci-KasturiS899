5fb241a9e8531db6d92884cff25c1fba316f4e50
Kasturi <75318338+KasturiS899@users.noreply.github.com>
2023-07-18 12:11:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function fibonacci(num) {
 	let f3 = f1+f2;
 	while(f3 <=num){
 		if(num===f3){
-			return true;
+			return 1;
 		}
 		f1=f2;
 		f2=f3;
 		f3=f1+f2;
 	}
-	return false;
+	return 0;
 	
 }
 
