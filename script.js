1ca74972c2177c4444edcaca00763551c942f27a
Kasturi <75318338+KasturiS899@users.noreply.github.com>
2023-07-18 11:33:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
+	let f1 =0;
+	let f2 =1;
+	let f3 = f1+f2;
+	while(f3 <=num){
+		if(num===f3){
+			return true;
+		}
+		f1=f2;
+		f2=f3;
+		f3=f1+f2;
+	}
+	return false;
+	
 }
 
 module.exports = fibonacci;
