2daa293567f557009d2637e8faaee9e791899cae
Kasturi <75318338+KasturiS899@users.noreply.github.com>
2023-07-21 11:52:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function fibonacci(num) {
 // your code here
 	let prev =0;
 	let curr =0;
-	let next = prev+curr;
 
-	if(num==0) 
+	if(num==1) return prev;
+	if(num==2) return curr;
 		return prev;
 	for(let i=3;i<=num;i++){
 		let next=prev+curr;
